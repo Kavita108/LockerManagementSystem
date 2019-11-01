dda4369ddf915b951ee052bf71225f03c1bb3087
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2019-11-01 00:53:04 -0700

    cleanup program class

--- LockerManagementSystem/Program.cs
+++ LockerManagementSystem/Program.cs
@@ -6,18 +6,7 @@ namespace LockerManagementSystem
     {
         static void Main(string[] args)
         {
-            //object intialization
-            //instance of class (Locker) = = object
-
-            var locker = LockerManager.GetLocker("test@test.com", LockerSize.Medium);        
-
-         
-            /* String Interapolation*/
-            Console.WriteLine($" LS: {locker.LockerSize} , EA: {locker.EmailAddress}, CD: {locker.CheckingDate}, LN: {locker.LockerNumber}");
-
-            var locker2 = LockerManager.GetLocker("test1@test.com", LockerSize.Small);
-            Console.WriteLine($" LS: {locker2.LockerSize} , EA: {locker2.EmailAddress}, CD: {locker2.CheckingDate}, LN: {locker2.LockerNumber}");
-
+            
             
         }
     }
