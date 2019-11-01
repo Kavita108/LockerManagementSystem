37d7e835d5b1787d4b99a72042e929375b4e8ff3
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2019-11-01 01:22:19 -0700

    Added Menu System

--- LockerManagementSystem/Program.cs
+++ LockerManagementSystem/Program.cs
@@ -6,8 +6,41 @@ namespace LockerManagementSystem
     {
         static void Main(string[] args)
         {
-            
-            
+            Console.WriteLine("**************WELCOME! * ************");
+            while (true)
+            {
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Get A Locker");
+                Console.WriteLine("2.DropABag");
+                Console.WriteLine("3.PickABag");
+                Console.WriteLine("4.Close A Locker");
+                Console.WriteLine("5.Print Available Lockers");
+
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("Thankyou For Visiting");
+                        return;
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Please select a valid option !");
+                        break;
+
+                }
+            }
+
+
         }
     }
 }
