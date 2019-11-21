b351fff80c526eebcf4597aa9e887c02a673f079
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2019-11-20 23:40:09 -0800

    configuring database

---
+++ LockerManagementSystem/LockerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LockerManagementSystem
+{
+    class LockerContext : DbContext
+    {
+        public DbSet<Locker> Lockers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Locker>(entity =>
+           {
+               entity.HasKey(l => l.LockerID).HasName("PK_Lockers");
+
+               entity.Property(l => l.LockerID).ValueGeneratedOnAdd();
+
+               entity.Property(l => l.LockerSize).IsRequired();
+            });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog=Lockers;Integrated Security = True; Connect Timeout = 30;");        }
+        }
+}
